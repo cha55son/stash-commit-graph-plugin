e7e1ddefb71a1e031f2ab1b6ce61544bc017c4ed
Chason Choate <cha55son@gmail.com>
2014-05-08 23:31:17 -0500

    Once the tab is loaded it makes a call back to the REST API for the first 20 commits. First steps at implementing a graph.

--- src/main/resources/js/commitgraph.js
+++ src/main/resources/js/commitgraph.js
@@ -1,1 +1,27 @@
-alert('hi');
+(function($) {
+    $(document).ready(function() {
+        if (!CommitGraph) return;
+        var url = '/rest/api/1.0/projects/' + CommitGraph.projectKey + '/repos/' + CommitGraph.repoSlug + '/commits';
+        $.ajax({
+            url: url,
+            data: {
+                limit: 20
+            }, 
+            success: function(data, status) {
+                var commits = data.values;
+                var data = [];
+                $.each(commits, function(i, commit) {
+
+                });
+                data.push(["10c2993a6ba2", [0, 1], [[0, 0, 1]]]);
+                data.push(["e5f3e2cf9be5", [0, 1], [[0, 0, 1]]]);
+                $('#commit-graph').commits({
+                    width: 400,
+                    height: 600,
+                    orientation: 'vertical',
+                    data: data
+                });
+            }
+        });
+    });
+})(jQuery);
